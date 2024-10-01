623fb152aeac006e6cf29f9625018bf5a1b91d4b
Said Zuhair <m.masoud247@gmail.com>
2024-10-01 23:21:47 +0200

    add scala support

---
+++ queries/scala/rainbow-delimiters.scm
@@ -0,0 +1,39 @@
+(parameters
+  "(" @delimiter
+  ")" @delimiter @sentinel) @container
+
+(bindings
+  "(" @delimiter
+  ")" @delimiter @sentinel) @container
+
+(arguments
+  "(" @delimiter
+  ")" @delimiter @sentinel) @container
+
+(type_arguments
+  "[" @delimiter
+  "]" @delimiter @sentinel) @container
+
+(type_parameters
+  "[" @delimiter
+  "]" @delimiter @sentinel) @container
+
+(tuple_type
+  "(" @delimiter
+  ")" @delimiter @sentinel) @container
+
+(tuple_expression
+  "(" @delimiter
+  ")" @delimiter @sentinel) @container
+
+(tuple_pattern
+  "(" @delimiter
+  ")" @delimiter @sentinel) @container
+
+(block
+  "{" @delimiter
+  "}" @delimiter @sentinel) @container
+
+(template_body
+  "{" @delimiter
+  "}" @delimiter @sentinel) @container
